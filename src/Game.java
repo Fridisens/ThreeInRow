8456be76e7144e3c6d19d3c0e349b66258db615c
Fridisens <fridisens@gmail.com>
2023-10-05 12:37:34 +0200

    Fifth commit

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,55 @@
+import java.util.Scanner;
+
 public class Game {
 
+    public void play() {
+        Scanner inputFromPlayer = new Scanner(System.in);
 
+        // Skapa en instans av Board
+        Board board = new Board();
 
+        System.out.println("Welcome to TicTacToe");
+        System.out.println();
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*");
+        System.out.println();
 
+        // Anropa metoden för att skriva ut spelbrädet
+        board.outprintBoard();
+
+        // Spelarnas tur att göra ett drag
+        boolean playerXTurn = true;
+
+        while (true) {
+            System.out.println();
+
+            System.out.println("Choose a number between 1-9 to make a move");
+
+            try {
+                int move = inputFromPlayer.nextInt();
+
+                if (move >= 1 && move <= 9 && board.isCellEmpty(move)) {
+                    char symbol = playerXTurn ? 'X' : 'O';
+                    board.makeAMove(move, symbol);
+
+                    board.outprintBoard();
+
+                    if (board.checkIfWin(symbol)) {
+                        System.out.println("Player " + symbol + " wins");
+                        break;
+                    } else if (board.isBoardFull()) {
+                        System.out.println("It's a draw, better luck next time");
+                        break;
+                    }
+
+                    playerXTurn = !playerXTurn;
+                } else {
+                    System.out.println("Invalid move, Try again");
+                }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Invalid input, try again to enter a number between 1-9");
+                inputFromPlayer.next();
+            }
+        }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,13 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner inputFromPlayer = new Scanner(System.in);
-
-        //Create instance of Board
-        Board board = new Board();
-
-        System.out.println("Welcome to TicTacToe");
-        System.out.println();
-        System.out.println("*-*-*-*-*-*-*-*-*-*-*");
-        System.out.println();
-
-        // Call method to print out board
-        board.outprintBoard();
-
-        //Players turn to make a move
-        boolean playerXTurn = true;
-
-        while (true) {
-            System.out.println();
-
-            System.out.println("Choose a number between 1-9 to make a move");
-
-            try {
-                int move = inputFromPlayer.nextInt();
-
-                if (move >= 1 && move <= 9 && board.isCellEmpty(move)) {
-                    char symbol = playerXTurn ? 'X' : 'O';
-                    board.makeAMove(move, symbol);
-
-                    board.outprintBoard();
-
-                    if (board.checkIfWin(symbol)) {
-                        System.out.println("Player " + symbol + " wins");
-                        break;
-                    } else if (board.isBoardFull()) {
-                        System.out.println("It´s a draw, better luck next time");
-                        break;
-
-                    }
-                    //Toggle the players turn between X and O
-                    playerXTurn = !playerXTurn;
-
-                } else {
-                    System.out.println("Invalid move, Try again");
-
-                }
-            //If player press wrong letter or other symbol
-            } catch (java.util.InputMismatchException e) {
-                System.out.println("Invalid input, try again to enter a number 1-9");
-                inputFromPlayer.next();
-            }
-
-
-        }
-
-
+        Game game = new Game();
+        game.play();
     }
 }
 
-//Try catch statement for dealing with if the user print some other than 1-9
+
+
+
 
 
 
